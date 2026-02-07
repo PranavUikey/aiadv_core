e2f55f1d9f836c11c9ffee0d593352507073bedb
pranav <pranavuiih@gmail.con>
2026-02-07 16:55:02 +0530

    checking port

--- mailer.py
+++ mailer.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SMTP_SERVER = os.getenv("SMTP_HOST") or os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
+SMTP_PORT = 587
 SMTP_EMAIL = os.getenv("SMTP_EMAIL")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 
